02ac75d57569bb67c02c398f07b616a88a3c5a61
Oliver Adams <oliver.adams@gmail.com>
2014-03-01 16:03:52 +1100

    Added VideoMetadataActivity with no functionality

--- Aikuma/src/org/lp20/aikuma/ui/VideoMetadataActivity.java
+++ Aikuma/src/org/lp20/aikuma/ui/VideoMetadataActivity.java
@@ -1,5 +1,11 @@
 package org.lp20.aikuma.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import java.util.UUID;
+import org.lp20.aikuma.R;
+
 /**
  * The activity that allows metadata about a video to be entered by the user.
  *
@@ -7,4 +13,17 @@ package org.lp20.aikuma.ui;
  * @author	Florian Hanke	<florian.hanke@gmail.com>
  */
 public class VideoMetadataActivity extends AikumaListActivity {
+
+	private UUID uuid;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.video_metadata);
+		safeActivityTransition = true;
+
+		Intent intent = getIntent();
+		uuid = UUID.fromString(
+				(String) intent.getExtras().get("uuidString"));
+	}
 }
